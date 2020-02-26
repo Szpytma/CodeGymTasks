0a36f878fb97e191c02d8f4193bda67e025e40b1
szpytma <szpytmapawel@hotmail.com>
2020-02-26 13:18:57 +0000

    commit

--- 1.JavaSyntax/src/pl/codegym/task/task08/task0827/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task08/task0827/Solution.java
@@ -1,6 +1,8 @@
 package pl.codegym.task.task08.task0827;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /* 
 Praca z datami
@@ -9,9 +11,13 @@ Praca z datami
 public class Solution {
     public static void main(String[] args) {
         System.out.println(isDataNieparzysta("MAY 1 2013"));
+        System.out.println(isDataNieparzysta("MAY 2 2013"));
     }
 
     public static boolean isDataNieparzysta(String date) {
-        return true;
+
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(new Date(date));
+        return calendar.get(Calendar.DAY_OF_YEAR) % 2 != 0;
+        }
     }
-}
