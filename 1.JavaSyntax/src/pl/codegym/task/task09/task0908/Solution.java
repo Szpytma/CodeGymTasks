a7679837cbcff7e442c89f7c96db7acfa64025a4
Szpytma Pawel <szpytmapawel@hotmail.com>
2020-02-28 14:52:14 +0000

    commit

---
+++ 1.JavaSyntax/src/pl/codegym/task/task09/task0908/Solution.java
@@ -0,0 +1,16 @@
+package pl.codegym.task.task09.task0908;
+
+/* 
+Wyjątki przy pracy z ciągami
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+
+        String s = null;
+        String m = s.toLowerCase();
+
+        //tutaj wpisz swój kod
+    }
+}
