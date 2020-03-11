89b756619da43bebf53bd7d681accd00cf532e81
szpytma <szpytmapawel@hotmail.com>
2020-03-11 12:22:51 +0000

    commit

--- 2.JavaCore/src/pl/codegym/task/task12/task1224/Solution.java
+++ 2.JavaCore/src/pl/codegym/task/task12/task1224/Solution.java
@@ -5,32 +5,52 @@ Nieznane zwierzę
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println(getObjectType(new Cat()));
-        System.out.println(getObjectType(new Tiger()));
-        System.out.println(getObjectType(new Lion()));
-        System.out.println(getObjectType(new Bull()));
-        System.out.println(getObjectType(new Pig()));
+
+  public static void main(String[] args) {
+    System.out.println(getObjectType(new Cat()));
+    System.out.println(getObjectType(new Tiger()));
+    System.out.println(getObjectType(new Lion()));
+    System.out.println(getObjectType(new Bull()));
+    System.out.println(getObjectType(new Pig()));
+  }
+
+  public static String getObjectType(Object o) {
+    //tutaj wpisz swój kod
+    if (o instanceof Cat) {
+      return o.getClass().getSimpleName();
+    }
+    if (o instanceof Tiger) {
+      return o.getClass().getSimpleName();
+    }
+    if (o instanceof Lion) {
+      return o.getClass().getSimpleName();
+    }
+    if (o instanceof Bull) {
+      return o.getClass().getSimpleName();
+    } else {
+      return "Animal";
     }
 
-    public static String getObjectType(Object o) {
-        //tutaj wpisz swój kod
 
-        return "Animal";
-    }
+  }
 
-    public static class Cat {
-    }
+  public static class Cat {
 
-    public static class Tiger {
-    }
+  }
 
-    public static class Lion {
-    }
+  public static class Tiger {
 
-    public static class Bull {
-    }
+  }
 
-    public static class Pig {
-    }
+  public static class Lion {
+
+  }
+
+  public static class Bull {
+
+  }
+
+  public static class Pig {
+
+  }
 }
