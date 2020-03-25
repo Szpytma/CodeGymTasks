debfe16d781308129d0400077e607097879b97ef
Szpytma <56477986+Szpytma@users.noreply.github.com>
2020-03-25 21:31:52 +0000

    commit

---
+++ 2.JavaCore/src/pl/codegym/task/task15/task1523/Solution.java
@@ -0,0 +1,21 @@
+package pl.codegym.task.task15.task1523;
+
+/* 
+Przeciążanie konstruktorów
+*/
+
+public class Solution {
+    public Solution(String s) {
+    }
+    protected Solution(Integer i) {
+    }
+    private Solution() {
+    }
+    Solution(Double d) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/pl/codegym/task/task15/task1523/SubSolution.java
@@ -0,0 +1,16 @@
+package pl.codegym.task.task15.task1523;
+
+public class SubSolution extends Solution {
+
+    public SubSolution(String s) {
+        super(s);
+    }
+
+    protected SubSolution(Integer i) {
+        super(i);
+    }
+
+    SubSolution(Double d) {
+        super(d);
+    }
+}
