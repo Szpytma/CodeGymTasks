7b8807b54d37686d07a9984060164dc138e2082b
Szpytma <56477986+Szpytma@users.noreply.github.com>
2020-03-28 19:56:41 +0000

    commit

--- 2.JavaCore/src/pl/codegym/task/task15/task1529/Helicopter.java
+++ 2.JavaCore/src/pl/codegym/task/task15/task1529/Helicopter.java
@@ -4,5 +4,6 @@ public class Helicopter implements CanFly {
     @Override
     public void fly() {
 
+
     }
 }
--- 2.JavaCore/src/pl/codegym/task/task15/task1529/Solution.java
+++ 2.JavaCore/src/pl/codegym/task/task15/task1529/Solution.java
@@ -13,7 +13,6 @@ public class Solution {
     public static void main(String[] args) {
 
     }
-
     static {
         try {
             reset();
